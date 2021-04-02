9f8c0ce56013cc07d17616cb0b59b22a97e290ee
gjwehnes <gjwehnes@shaw.ca>
2021-04-02 13:45:45 -0600

    Update TriangleClassifierTest.java

--- src/TriangleClassifierTest.java
+++ src/TriangleClassifierTest.java
@@ -1,16 +1,24 @@
-//2019-10-02
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TriangleClassifierTest extends TestCase {
-    
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.*;
+
+public class TriangleClassifierTest  {
+
+    @Test
     public void testValidTriangle() {
     	assertEquals(true, TriangleClassifier.isValidTriangle(60,60,60));
     	assertEquals(false, TriangleClassifier.isValidTriangle(60,60,90));    	
     	assertEquals(false, TriangleClassifier.isValidTriangle(0,90,90));    	
     	assertEquals(false, TriangleClassifier.isValidTriangle(90,0,90));    	
-    	assertEquals(false, TriangleClassifier.isValidTriangle(0,90,90));    	
+    	assertEquals(false, TriangleClassifier.isValidTriangle(0,90,90));
     }
     
+    @Test
     public void testClassifyByAngle() {
        	assertEquals("acute", TriangleClassifier.classifyByAngle(60,60,60));    	
        	assertEquals("right", TriangleClassifier.classifyByAngle(90,60,30));    	
@@ -21,6 +29,7 @@ public class TriangleClassifierTest extends TestCase {
        	assertEquals("obtuse", TriangleClassifier.classifyByAngle(20,100,60));    	
     }
     
+    @Test
     public void testClassifyBySide() {
     	assertEquals("equilateral", TriangleClassifier.classifyBySide(60,60,60));	
     	assertEquals("isosceles", TriangleClassifier.classifyBySide(30,30,120));	
@@ -30,7 +39,8 @@ public class TriangleClassifierTest extends TestCase {
     	assertEquals("scalene", TriangleClassifier.classifyBySide(60,70,50));
     	assertEquals("scalene", TriangleClassifier.classifyBySide(70,50,60));
     }
-        
+    
+    @Test
     public void testRightScalene() {
         assertEquals("right scalene", TriangleClassifier.getClassification(30, 60, 90));
         assertEquals("right scalene", TriangleClassifier.getClassification(30, 90, 60));
@@ -40,12 +50,14 @@ public class TriangleClassifierTest extends TestCase {
         assertEquals("right scalene", TriangleClassifier.getClassification(90, 60, 30));
     }
     
+    @Test
     public void testRightIsosceles() {
         assertEquals("right isosceles", TriangleClassifier.getClassification(45, 45, 90));
         assertEquals("right isosceles", TriangleClassifier.getClassification(45, 90, 45));
         assertEquals("right isosceles", TriangleClassifier.getClassification(90, 45, 45));
     }
-
+    
+    @Test
     public void testObtuseIsosceles() {
         assertEquals("obtuse isosceles", TriangleClassifier.getClassification(100, 40, 40));
         assertEquals("obtuse isosceles", TriangleClassifier.getClassification(40, 100, 40));
@@ -53,6 +65,7 @@ public class TriangleClassifierTest extends TestCase {
         assertEquals("obtuse isosceles", TriangleClassifier.getClassification(1, 1, 178));
     }
     
+    @Test
     public void testObtuseScalene() {
         assertEquals("obtuse scalene", TriangleClassifier.getClassification(100, 60, 20));
         assertEquals("obtuse scalene", TriangleClassifier.getClassification(100, 20, 60));
@@ -61,17 +74,20 @@ public class TriangleClassifierTest extends TestCase {
         assertEquals("obtuse scalene", TriangleClassifier.getClassification(20, 100, 60));
         assertEquals("obtuse scalene", TriangleClassifier.getClassification(20, 60, 100));
     }
-
+    
+    @Test
     public void testEquilateral() {
         assertEquals("equilateral", TriangleClassifier.getClassification(60, 60, 60));
     }
-
+    
+    @Test
     public void testAcuteIsosceles() {
         assertEquals("acute isosceles", TriangleClassifier.getClassification(80, 50, 50));
         assertEquals("acute isosceles", TriangleClassifier.getClassification(50, 50, 80));
         assertEquals("acute isosceles", TriangleClassifier.getClassification(50, 80, 50));
     }
     
+    @Test
     public void testAcuteScalene() {
         assertEquals("acute scalene", TriangleClassifier.getClassification(50, 60, 70));
         assertEquals("acute scalene", TriangleClassifier.getClassification(50, 70, 60));
@@ -80,7 +96,8 @@ public class TriangleClassifierTest extends TestCase {
         assertEquals("acute scalene", TriangleClassifier.getClassification(70, 50, 60));
         assertEquals("acute scalene", TriangleClassifier.getClassification(70, 60, 50));
     }
-
+    
+    @Test
     public void testInvalid() {
         assertEquals("INVALID", TriangleClassifier.getClassification(30, 90, 90));
         assertEquals("INVALID", TriangleClassifier.getClassification(90, 30, 90));
@@ -90,4 +107,5 @@ public class TriangleClassifierTest extends TestCase {
         assertEquals("INVALID", TriangleClassifier.getClassification(0, 60, 10));        
     }
     
+   
 }
